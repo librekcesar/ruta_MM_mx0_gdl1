7e73d4609bddaaee623da6275bacbc8608f6d6bc
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-14 14:57:57 -0600

    Add files via upload

---
+++ estaciones_mx0_gdl1_copia.js
@@ -0,0 +1,9 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_gdl1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "20.624283851536905, -103.2893056", "description": "GDL1", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -103.2892967, 20.6243079, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "19.64220097106514, -99.14250999381147", "description": "MX0", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -99.1422607, 19.6422227, 0.0 ] } }
+]
+}
